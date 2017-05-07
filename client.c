c23d36140f23ff8c675a816091b45b8a44857798
Hubert Sulich <sulich.hubert@gmail.com>
2017-05-07 17:53:34 -0400

    Add multicasting functionality

--- client.c
+++ client.c
@@ -27,6 +27,9 @@ typedef struct Group_Info_ {
 	int multicast_group_port;
 } Group_Info;
 
+char group_ip[16], username[MAX_USERNAME_LENGTH + 1], group_name[MAX_GROUP_LENGTH + 1]; 
+int group_port;
+
 
 void err_exit() {
 	perror("chat_client");
@@ -52,15 +55,18 @@ int validate_request(char *request) {
 	if (strlen(groupname) > MAX_GROUP_LENGTH) {
 		return 0;
 	}
+
+	memset(&group_name, 0, sizeof(group_name));
+	sprintf(group_name, "%s", groupname);
 	return 1;
 }
 
 void *listen_to_group(void *arg) {
-	struct sockaddr_in addr;
+	struct sockaddr_in group_addr;
 	unsigned char ttl;
-	int addr_len, sfd, rec_mess_len, reuse;
+	int group_addr_len, sfd, rec_mess_len, reuse;
 	struct ip_mreq mreq;
-	char formatted_message[MAX_USERNAME_LENGTH + MAX_MESSAGE_LENGTH + 3];
+	char message[MAX_USERNAME_LENGTH + MAX_MESSAGE_LENGTH + 3];
 
 	if ((sfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 		err_exit();
@@ -71,46 +77,45 @@ void *listen_to_group(void *arg) {
 		err_exit();
 	}
 
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// addr.sin_port = htons(MULTICAST_GROUP_PORT);
-	addr_len = sizeof(addr);
+	memset(&group_addr, 0, sizeof(group_addr));
+	group_addr.sin_family = AF_INET;
+	group_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	group_addr.sin_port = htons(group_port);
+	group_addr_len = sizeof(group_addr);
 
-	if (bind(sfd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+	if (bind(sfd, (struct sockaddr *) &group_addr, group_addr_len) == -1) {
 		err_exit();
 	}
 
-	// mreq.imr_multiaddr.s_addr = inet_addr(MULTICAST_GROUP_IP);
-	// mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+	mreq.imr_multiaddr.s_addr = inet_addr(group_ip);
+	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
 	ttl = 255;
 	setsockopt(sfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
 
 	if (setsockopt(sfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) == -1) {
 		err_exit();
 	}
-	
+
 	while (1) {
-		memset(formatted_message, 0, sizeof(formatted_message));
-		if ((rec_mess_len = recvfrom(sfd, formatted_message, sizeof(formatted_message), 0, (struct sockaddr *) &addr, &addr_len)) == -1) {
+		memset(message, 0, sizeof(message));
+		if ((rec_mess_len = recvfrom(sfd, message, sizeof(message), 0, (struct sockaddr *) &group_addr, &group_addr_len)) == -1) {
 			err_exit();
 		}
 		pthread_mutex_lock(&chat_lock);
 		printf("\33[2K\r");
-		printf("%s", formatted_message);
+		printf("%s", message);
 		printf(">>> ");
 		fflush(stdout);
 		pthread_mutex_unlock(&chat_lock);
 	}
 }
 
-void join_group(char *name) {
+void join_group() {
 	struct sockaddr_in group_addr;
 	int sfd;
 	unsigned char ttl;
 	char message[MAX_MESSAGE_LENGTH + 1];
 	char formatted_message[MAX_USERNAME_LENGTH + MAX_MESSAGE_LENGTH + 3];
-	char *username = name;
 	pthread_t tid;
 
 	if ((sfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -119,11 +124,13 @@ void join_group(char *name) {
 	
 	memset(&group_addr, 0, sizeof(group_addr));
 	group_addr.sin_family = AF_INET;
-	// group_addr.sin_addr.s_addr = inet_addr(MULTICAST_GROUP_IP);
-	// goup_addr.sin_port = htons(MULTICAST_GROUP_PORT);
+	group_addr.sin_addr.s_addr = inet_addr(group_ip);
+	group_addr.sin_port = htons(group_port);
 
 	ttl = 255;
 	setsockopt(sfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
+
+	printf("Joined %s\n", group_name);
 
 	pthread_create(&tid, NULL, listen_to_group, NULL);
 
@@ -145,11 +152,10 @@ void join_group(char *name) {
 int main(int argc, char *argv[]) {
 	struct sockaddr_in client_addr, server_addr;
 	int sfd, valid_request;
-	char username[MAX_USERNAME_LENGTH + 1];
 	char request[strlen("create ") + MAX_GROUP_LENGTH + 1];
 	char formatted_request[MAX_USERNAME_LENGTH + sizeof(request) + 1];
 	char server_response[MAX_SERVER_RESPONSE_LENGTH + 1];
-	Group_Info *info_from_server;
+//	Group_Info *info_from_server;
 
 	if (argc != 2) {
 		printf("Usage: ./chat_app username\n");
@@ -177,18 +183,23 @@ int main(int argc, char *argv[]) {
 		err_exit();
 	}
 
+	int reuse = 1;
+	if(setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0) {
+		err_exit();
+	}
+
 	if (bind(sfd, (struct sockaddr *) &client_addr, sizeof(client_addr)) == -1) {
 		err_exit();
 	}
 
-	info_from_server = malloc(sizeof(Group_Info));
+//	info_from_server = malloc(sizeof(Group_Info));
 
 	while (1) {
 		printf("Options:\n\tcreate <groupname>\n\tjoin <groupname>\n\tget groupnames\n");
 		printf(">>> ");
 		memset(&request, 0, sizeof(request));
 		memset(&formatted_request, 0, sizeof(formatted_request));
-		memset(&info_from_server, 0, sizeof(info_from_server));
+//		memset(&info_from_server, 0, sizeof(info_from_server));
 		memset(&server_response, 0, sizeof(server_response));
 		fgets(request, sizeof(request), stdin);
 		if ((valid_request = validate_request(request)) == 1) {
@@ -197,8 +208,14 @@ int main(int argc, char *argv[]) {
 			// TODO: handle sendto and recvfrom errors
 			sendto(sfd, formatted_request, sizeof(formatted_request), 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
 			
-			recvfrom(sfd, info_from_server, sizeof(info_from_server), 0, NULL, NULL);
-			printf("%s:%d\n", info_from_server->multicast_group_addr, info_from_server->multicast_group_port);
+			recvfrom(sfd, server_response, sizeof(server_response), 0, NULL, NULL);
+			memset(&group_ip, 0, sizeof(group_ip));
+			char *temp = malloc(sizeof(server_response));
+			sprintf(temp, "%s", server_response);
+			sprintf(group_ip, "%s", strsep(&temp, " "));
+			group_port = atoi(strsep(&temp, " "));
+
+			join_group();
 		}
 		else if (valid_request == 0) {
 			printf("Group name is too long, must be 50 characters or less\n");
